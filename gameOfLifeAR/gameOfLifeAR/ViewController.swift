54b2bd8a0cfa6cadb5827f961cb971ff7e082869
Tamara <tamara.erlij@gmail.com>
2019-11-01 15:25:05 -0300

    first commit

---
+++ gameOfLifeAR/gameOfLifeAR/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  gameOfLifeAR
+//
+//  Created by Tamara Erlij on 01/11/19.
+//  Copyright © 2019 Tamara Erlij. All rights reserved.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
